3207b601879b46046385037e38c7b973b63d43a4
PuddingCoke <zhangxianzhe1231@outlook.com>
2024-04-13 22:43:57 +0800

    增加了曼德尔球示例

---
+++ MandelBulb/main.cpp
@@ -0,0 +1,19 @@
+#include<Gear/Gear.h>
+
+#include"MyGame.h"
+
+using EngineUsage = Configuration::EngineUsage;
+
+int main(int argc, const char* argv[])
+{
+	Configuration config(1920, 1080, L"RayTracing", EngineUsage::NORMAL);
+
+	Gear::initialize();
+
+	if (!Gear::get()->iniEngine(config, argc, argv))
+	{
+		Gear::get()->iniGame(new MyGame());
+	}
+
+	Gear::release();
+}
