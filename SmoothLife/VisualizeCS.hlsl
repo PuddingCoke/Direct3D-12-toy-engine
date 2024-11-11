b43c76e6f6eadd2cab13c9985579bc012f3a2ae1
PuddingCoke <zhangxianzhe1231@outlook.com>
2024-11-11 22:39:44 +0800

    完成了SmoothLife和MandelBrotSet现在要为MandelBrotSet找到合适的渐变色

---
+++ SmoothLife/VisualizeCS.hlsl
@@ -0,0 +1,15 @@
+cbuffer TextureIndices : register(b2)
+{
+    uint outputTextureIndex;
+    uint readTextureIndex;
+}
+
+static RWTexture2D<float4> outputTexture = ResourceDescriptorHeap[outputTextureIndex];
+
+static Texture2D<float> readTexture = ResourceDescriptorHeap[readTextureIndex];
+
+[numthreads(16, 9, 1)]
+void main(const uint2 DTid : SV_DispatchThreadID)
+{
+    outputTexture[DTid] = float4(readTexture[DTid].rrr, 1.0);
+}
---
+++ SmoothLife/WhiteNoiseCS.hlsl
@@ -0,0 +1,44 @@
+#include"Common.hlsli"
+
+cbuffer TextureIndices : register(b2)
+{
+    uint outputTextureIndex;
+    uint uintSeed;
+}
+
+static RWTexture2D<float> outputTexture = ResourceDescriptorHeap[outputTextureIndex];
+
+uint hash(uint2 x)
+{
+    const uint m = 0x5bd1e995U;
+    uint hash = uintSeed;
+    // process first vector element
+    uint k = x.x;
+    k *= m;
+    k ^= k >> 24;
+    k *= m;
+    hash *= m;
+    hash ^= k;
+    // process second vector element
+    k = x.y;
+    k *= m;
+    k ^= k >> 24;
+    k *= m;
+    hash *= m;
+    hash ^= k;
+	// some final mixing
+    hash ^= hash >> 13;
+    hash *= m;
+    hash ^= hash >> 15;
+    return hash;
+}
+
+[numthreads(16, 9, 1)]
+void main(const uint2 DTid : SV_DispatchThreadID )
+{
+    uint hashValue = hash(DTid);
+    
+    float value = float(hashValue) / float(0xffffffffU);
+    
+    outputTexture[DTid] = value * 0.75;
+}
