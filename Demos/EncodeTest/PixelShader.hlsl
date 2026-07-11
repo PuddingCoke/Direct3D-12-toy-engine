59bd6dd11d9c09fec007eaf16157f6876504a042
PuddingCoke <zhangxianzhe1231@outlook.com>
2026-07-12 03:30:57 +0800

    EncodeTest项目已完成它的使命，SteeringBehavior项目现在将作为视频编码模块的测试项目

--- Demos/EncodeTest/PixelShader.hlsl
+++
@@ -1,7 +0,0 @@
-#include"Common.hlsli"
-
-float4 main() : SV_TARGET
-{
-    return float4(cos(perframeResource.timeElapsed) * 0.5 + 0.5,
-    sin(perframeResource.timeElapsed) * 0.5 + 0.5, 1.0f, 1.0f);
-}
